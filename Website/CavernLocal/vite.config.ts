1ba0ce15881fc1caadac438cfcd1df94ba2a4ce7
waynelai1228 <waynelai01011231@gmail.com>
2025-03-28 03:59:56 -0700

    Customized vite + react template with react router

---
+++ Website/CavernLocal/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import { reactRouter } from "@react-router/dev/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [reactRouter()],
+})
