aaae36da0b028fb6faa89fa7259056b4ed901deb
waynelai1228 <waynelai01011231@gmail.com>
2025-07-02 20:54:06 -0700

    Update test to remove MockBean marked for deprecation

--- CavernService/src/test/java/com/cavernservice/controller/ProjectControllerTest.java
+++ CavernService/src/test/java/com/cavernservice/controller/ProjectControllerTest.java
@@ -16,21 +16,32 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(ProjectController.class)
 @ContextConfiguration(classes = CavernServiceApplication.class)
+@Import(ProjectControllerTest.MockConfig.class)
 public class ProjectControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private ProjectRepository projectRepository;
+
+    @TestConfiguration
+    static class MockConfig {
+        @Bean
+        public ProjectRepository projectRepository() {
+            return mock(ProjectRepository.class);
+        }
+    }
 
     private Project sampleProject;
     private UUID sampleId;
